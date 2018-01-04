0025f5dac19d76e4218560075c21f034a6683321
madhumathi1 <madhumathid28@gmail.com>
2018-01-04 05:48:38 -0800

    Create help.c

---
+++ help.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+int a,b,c;
+scanf("%d",&a);
+scanf("%d",&b);
+scanf("%d",&c);
+if((a>b)&&(a>c))
+{
+    printf("a is greatest");
+}
+    else if((b>a)&&(b>c))
+    {
+    printf("b is greatest");
+}
+    else
+    {
+    printf("c is greatest");
+}
+return 0;
+}
